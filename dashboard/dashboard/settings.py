c7a0f9b9d92a6baf5fae64ac35bd0a8cda3d148a
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-01-13 21:07:11 +0100

    Changing secret key

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!yln=v56+u*8r%%w=ry)b@ar)q@bl)-*yolwc)vj3qe@h#9mtt'
+import os
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
