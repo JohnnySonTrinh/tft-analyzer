82193a9c0a469108ae84d1c06cb10ddecf7d9c58
Johnny Trinh <johnny.trinh@hotmail.se>
2024-01-13 14:11:17 +0100

    Connect request for player stats

--- stats/urls.py
+++ stats/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),       # Root URL for 'stats' app
     path('home/', views.home, name='home'),  # '/home' URL for 'stats' app
+    path('search/', views.search_results, name='search_results'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,6 +1,12 @@
 # stats/views.py
 
 from django.shortcuts import render
+from .models import PlayerStats
 
 def home(request):
     return render(request, 'stats/home.html')
+
+def search_results(request):
+    query = request.GET.get('player_name')
+    results = PlayerStats.objects.filter(player_name__icontains=query)
+    return render(request, 'search_results.html', {'results': results})
